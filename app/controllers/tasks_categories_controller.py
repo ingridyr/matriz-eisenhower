ba709c2b080dcc8b6fa8202831e2d05097e17429
Ingridy Rodrigues <ingridyrodrigues.f@hotmail.com>
2022-04-28 15:33:24 -0300

    feat: create function get all

--- app/controllers/tasks_categories_controller.py
+++ app/controllers/tasks_categories_controller.py
@@ -1,11 +1,36 @@
+from flask import jsonify
 from app.models.categories_model import Category
 from app.models.tasks_model import Task
+from app.models.eisenhowers_model import Eisenhower
 from app.models.tasks_categories_table import tasks_categories
 
 from app.configs.database import db
 
 def retrieve_all():
-    query = db.session.query(Category, Task).select_from(Category).join(tasks_categories).join(Task).all()
-    print(query)
+    #query = db.session.query(Category, Task).select_from(Category).join(tasks_categories).join(Task).all()
+    query = db.session.query(Category).all()
+    list_categories = []
 
-    return ''
+    for index in range(len(query)):
+
+        category = {
+            "id": query[index].id,
+            "name": query[index].name,
+            "description": query[index].description,
+            "tasks": []
+        }
+
+        for item in query[index].tasks:
+            task = {
+                "id": item.id,
+                "name": item.name,
+                "description": item.description,
+                "classification": (Eisenhower.query.get(item.eisenhower_id).type)
+            }
+            category["tasks"].append(task)
+
+        list_categories.append(category)
+
+    #print(query)
+
+    return jsonify(list_categories)
--- app/models/categories_model.py
+++ app/models/categories_model.py
@@ -4,6 +4,7 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 
 from app.models.tasks_categories_table import tasks_categories
+from app.models.tasks_model import Task
 
 from dataclasses import dataclass
 
@@ -12,11 +13,13 @@ class Category(db.Model):
     id: int
     name: str
     description: str
+    tasks: Task
 
     __tablename__ = "categories"
 
     id = Column(Integer, primary_key=True)
     name = Column(String(100), nullable=False, unique=True)
     description = Column(String)
+
 
     tasks = relationship("Task", secondary=tasks_categories, back_populates="categories")
